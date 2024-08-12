6c3ce84ccaa205c90ac984ae3c24b22c556ac9b2
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2024-08-13 05:20:04 +0600

    Dynamic Nav Section For PC WP Theme

--- functions.php
+++ functions.php
@@ -20,28 +20,46 @@ function sab_pc_css_js_file_calling()
     wp_enqueue_style('sab_pc_style', get_stylesheet_uri());
 
     //bootstrap.css file
-    wp_register_style('sab_pc_bootstrap_style', get_template_directory_uri() . 'css/bootstrap.css');
+    wp_register_style('sab_pc_bootstrap_style', get_template_directory_uri() . '/css/bootstrap.css');
     wp_enqueue_style('sab_pc_bootstrap_style');
 
     //custom.css file
-    wp_register_style('sab_pc_custom_style', get_template_directory_uri() . 'css/custom.css');
+    wp_register_style('sab_pc_custom_style', get_template_directory_uri() . '/css/custom.css');
     wp_enqueue_style('sab_pc_custom_style');
 
     //jquery
     wp_enqueue_script('jquery');
 
     // bootstrap.js
-    wp_register_script('sab_pc_bootstrap_js', get_template_directory_uri() . 'javascript/bootstrap.js', array(), '5.0.2', true);
+    wp_register_script('sab_pc_bootstrap_js', get_template_directory_uri() . '/javascript/bootstrap.js', array(), '5.0.2', true);
     wp_enqueue_script('sab_pc_bootstrap_js');
     // custom.js
-    wp_register_script('sab_pc_custom_js', get_template_directory_uri() . 'javascript/custom.js', array(), '1.0.0', true);
+    wp_register_script('sab_pc_custom_js', get_template_directory_uri() . '/javascript/custom.js', array(), '1.0.0', true);
     wp_enqueue_script('sab_pc_custom_js');
+
+
+    // // Google Fonts Enqueue
+
+    // wp_enqueue_style('sab_pc_google_fonts', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald:wght@200..700&display=swap', false);
 
 
 }
 
 
-//Function for Customizer
+// Separately adding/Enqueueing Google Fonts 
+function sab_pc_google_fonts()
+{
+
+    wp_enqueue_style('sab_pc_google_f', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald:wght@200..700&display=swap', false);
+
+}
+
+add_action('wp_enqueue_scripts', 'sab_pc_google_fonts');
+
+
+
+
+//Functions for Customizer
 
 add_action('customize_register', 'sab_pc_customizer_register');
 
@@ -84,6 +102,48 @@ function sab_pc_customizer_register($wp_customize)
 
 
 
+    // Navigation Menu Positioning in Customizer
+
+
+    $wp_customize->add_section(
+        'sab_pc_nav_menu_option',
+        array(
+
+            'title' => __('Menu Positioning Option', 'sabbir_noyon'),
+            'description' => 'If you wants to change navigation menu position, you can do it here!'
+
+
+        )
+    );
+
+    $wp_customize->add_setting(
+        'sab_pc_menu_position',
+        array(
+
+            'default' => 'right_menu'
+
+
+        )
+    );
+
+    $wp_customize->add_control(
+        'sab_pc_menu_position',
+        array(
+            'section' => 'sab_pc_nav_menu_option',
+            'setting' => 'sab_pc_menu_position',
+            'label' => 'Position The Navigation Menu',
+            'description' => 'You can change menu position from here!',
+            'type' => 'radio',
+            'choices' => array(
+                'left_menu' => 'Left Menu',
+                'right_menu' => 'Right Menu',
+                'center_menu' => 'Center Menu'
+            ),
+
+
+        )
+    );
+
 
 
 }
@@ -94,59 +154,10 @@ function sab_pc_customizer_register($wp_customize)
 
 
 
-// BY CHATGPT
 
-// add_theme_support('title-tag');
 
-// // CSS & JS file Calling Here
-// add_action('wp_enqueue_scripts', 'sab_pc_css_js_file_calling');
 
-// function sab_pc_css_js_file_calling() {
-//     // style.css file
-//     wp_enqueue_style('sab_pc_style', get_stylesheet_uri());
 
-//     // bootstrap.css file
-//     wp_register_style('sab_pc_bootstrap_style', get_template_directory_uri() . '/css/bootstrap.css');
-//     wp_enqueue_style('sab_pc_bootstrap_style');
+// Register Nav Menu
 
-//     // custom.css file
-//     wp_register_style('sab_pc_custom_style', get_template_directory_uri() . '/css/custom.css');
-//     wp_enqueue_style('sab_pc_custom_style');
-
-//     // jquery
-//     wp_enqueue_script('jquery');
-
-//     // bootstrap.js
-//     wp_register_script('sab_pc_bootstrap_js', get_template_directory_uri() . '/javascript/bootstrap.js', array(), '5.0.2', true);
-//     wp_enqueue_script('sab_pc_bootstrap_js');
-
-//     // custom.js
-//     wp_register_script('sab_pc_custom_js', get_template_directory_uri() . '/javascript/custom.js', array(), '1.0.0', true);
-//     wp_enqueue_script('sab_pc_custom_js');
-// }
-
-// // Function for Customizer
-// add_action('customize_register', 'sab_pc_customizer_register');
-
-// function sab_pc_customizer_register($wp_customize) {
-//     // Add Section
-//     $wp_customize->add_section('sab_pc_header_area', array(
-//         'title'       => __('Header Area', 'sabbir_Noyon'),
-//         'description' => 'Customize the header area',
-//         // 'priority'    => 30,
-//     ));
-
-//     // Add Setting
-//     $wp_customize->add_setting('sab_pc_header_text', array(
-//         'default'           => 'Default Header Text',
-//         'sanitize_callback' => 'sanitize_text_field',
-//     ));
-
-//     // Add Control
-//     $wp_customize->add_control('sab_pc_header_text_control', array(
-//         'label'    => __('Header Text', 'sabbir_Noyon'),
-//         'section'  => 'sab_pc_header_area',
-//         'settings' => 'sab_pc_header_text',
-//         'type'     => 'text',
-//     ));
-// }
+register_nav_menu('main_menu', __('main_menu', 'sabbir_Noyon'));
--- index.php
+++ index.php
@@ -13,19 +13,55 @@ This is Header Area
 
 
     <?php wp_head(); ?>
+
+
 </head>
 
 <body <?php body_class(); ?>>
 
-    <div id="header-area">
+    <div id="header_area" class="<?php echo get_theme_mod('sab_pc_menu_position') ?>">
         <div class="container">
-            <div class="col-md-3">
-                <a href="">
-                    <img src="<?php echo get_theme_mod('sab_pc_logo') ?>" alt="logo">
-                </a>
+            <div class="row">
+                <div class="col-md-3">
+                    <a>
+                        <img src="<?php echo get_theme_mod('sab_pc_logo') ?>" alt="logo">
+                    </a>
 
-            </div>
-            <div class="col-md-9">
+                </div>
+                <div class="col-md-9">
+                    <?php wp_nav_menu(
+                        array(
+                            'theme_location' => 'main_menu',
+                            'menu_id' => 'nav'
+                        )
+                    ) ?>
+
+                </div>
+
+                <!-- <ul id="nav">
+                   
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Media</a></li>
+                    <li>
+                        <a href="#">Services</a>
+                        <ul>
+                            <li><a href="#">Dropdown Menu</a></li>
+                            <li><a href="#">Dropdown Menu</a></li>
+                            <li><a href="#"> Nested Dropdown Menu</a></li>
+                            <ul>
+                                <li><a href="#">Nested Dropdown Menu</a></li>
+                                <li><a href="#">Nested Dropdown Menu</a></li>
+                                <li><a href="#">Nested Dropdown Menu</a></li>
+                            </ul>
+                            <li><a href="#">Dropdown Menu</a></li>
+                            <li><a href="#">Dropdown Menu</a></li>
+                            <li><a href="#">Dropdown Menu</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Download</a></li>
+                </ul> -->
 
             </div>
         </div>
